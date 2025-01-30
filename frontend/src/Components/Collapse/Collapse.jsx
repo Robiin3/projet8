565a0f78ece4f29dbd0813d839a18d4570d63631
Robiin <robin.molina@hotmail.fr>
2025-01-30 16:08:35 +0100

    collapse ok

--- frontend/src/Components/Collapse/Collapse.jsx
+++ frontend/src/Components/Collapse/Collapse.jsx
@@ -4,9 +4,18 @@ import { ArrowBackIos } from "@mui/icons-material";
 
 const Collapse = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
 
   const toggleCollapse = () => {
-    setIsOpen(!isOpen);
+    if (isOpen) {
+      setIsClosing(true);
+      setTimeout(() => {
+        setIsOpen(false);
+        setIsClosing(false);
+      }, 600); // Durée de l'animation de fermeture
+    } else {
+      setIsOpen(true);
+    }
   };
 
   return (
@@ -15,7 +24,9 @@ const Collapse = ({ title, children }) => {
         <h2 className="collapse-title">{title}</h2>
         <ArrowBackIos className={`collapse-chevron ${isOpen ? "open" : "closed"}`} />
       </div>
-      {isOpen && <div className="collapse-content">{children}</div>}
+      <div className={`collapse-content ${isOpen ? "animate" : ""}`}>
+        <p className={isClosing ? "closing" : ""}>{children}</p>
+      </div>
     </div>
   );
 };
