4dc180cf1f30a064b63313007e198019e0fad7d2
Robiin <robin.molina@hotmail.fr>
2025-01-30 15:10:59 +0100

    réglage collapse

--- frontend/src/Components/Collapse/Collapse.jsx
+++ frontend/src/Components/Collapse/Collapse.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Collapse.css';
+import { ArrowBackIos } from "@mui/icons-material";
 
 const Collapse = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,7 +13,7 @@ const Collapse = ({ title, children }) => {
     <div className="collapse">
       <div className="collapse-header" onClick={toggleCollapse}>
         <h2 className="collapse-title">{title}</h2>
-        <span className={`collapse-chevron ${isOpen ? 'open' : ''}`}>▼</span>
+        <ArrowBackIos className={`collapse-chevron ${isOpen ? "open" : "closed"}`} />
       </div>
       {isOpen && <div className="collapse-content">{children}</div>}
     </div>
--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -14,16 +14,16 @@ const About = () => {
       <BannerAbout />
       <div className="about-content">
         <Collapse title="Fiabilité">
-          <p>Texte</p>
+          <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
         </Collapse>
         <Collapse title="Respect">
-          <p>Texte</p>
+          <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
         </Collapse>
         <Collapse title="Service">
-          <p>Texte</p>
+          <p>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</p>
         </Collapse>
         <Collapse title="Sécurité">
-          <p>Texte</p>
+          <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
         </Collapse>
       </div>
     </div>
