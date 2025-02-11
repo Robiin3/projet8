38091e42291fda8dc1396b88c5757a88938f4087
Robiin <robin.molina@hotmail.fr>
2025-02-11 17:45:20 +0100

    footer en style et non en image

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Footer.css';
-import FooterLogo from '../../assets/footer-desktop.png';
+import FooterLogo from '../../assets/Logo-footer.png';
 
 function Footer() {
     return (
         <footer>
             <img src={FooterLogo} alt="Logo Footer" className="footer-logo" />
+            <p>© 2020 Kasa. All rights reserved</p>
         </footer>
     );
 }
