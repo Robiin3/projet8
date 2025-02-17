4a230aff498524d65c76bfbed992dd69f2c7cd34
Robiin <robin.molina@hotmail.fr>
2025-02-17 13:53:20 +0100

    flèches et compteur carrousel seulement plusieurs images

--- frontend/src/Components/Carousel/Carousel.jsx
+++ frontend/src/Components/Carousel/Carousel.jsx
@@ -18,19 +18,25 @@ const Carousel = ({ images }) => { // Carrousel d'images
 
   return ( // Affiche le carrousel avec les images et les boutons de navigation
     <div className="carousel">
-      {/* Bouton pour l'image précédente */}
-      <button onClick={handlePrev} className="carousel-button"> {/* Appelle la fonction handlePrev lors du clic */}
-        <img src={VectorLeft} alt="Previous" /> {/* Icône flèche gauche */}
-      </button>
+      {images.length > 1 && ( // Affiche les flèches seulement s'il y a plus d'une image
+        <>
+          {/* Bouton pour l'image précédente */}
+          <button onClick={handlePrev} className="carousel-button"> {/* Appelle la fonction handlePrev lors du clic */}
+            <img src={VectorLeft} alt="Previous" /> {/* Icône flèche gauche */}
+          </button>
+          {/* Bouton pour l'image suivante */}
+          <button onClick={handleNext} className="carousel-button">
+            <img src={VectorRight} alt="Next" /> {/* Icône flèche droite */}
+          </button>
+        </>
+      )}
       <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} className="carousel-image" /> {/* Affiche l'image actuelle en fonction de l'index */}
-      {/* Bouton pour l'image suivante */}
-      <button onClick={handleNext} className="carousel-button">
-        <img src={VectorRight} alt="Next" /> {/* Icône flèche droite */}
-      </button>
       {/* Compteur d'images */}
-      <div className="carousel-counter"> 
-        {currentIndex + 1}/{images.length} {/* Affiche l'index de l'image actuelle et le nombre total d'images */}
-      </div>
+      {images.length > 1 && ( // Affiche le compteur seulement s'il y a plus d'une image
+        <div className="carousel-counter"> 
+          {currentIndex + 1}/{images.length} {/* Affiche l'index de l'image actuelle et le nombre total d'images */}
+        </div>
+      )}
     </div>
   );
 };
